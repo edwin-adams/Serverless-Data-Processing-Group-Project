f67caa5b4eabcb55e031da41b00f90985a32ce01
Mohit Rajeshkumar Sojitra <mh546676@dal.ca>
2023-06-02 00:04:59 -0300

    initial commit of react project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <button class='btn btn-primary'>TEsting</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
