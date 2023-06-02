c9986f5c56b3bb06de23fa74050d7c9cb1857bd1
Harshil Kalpeshkumar Shah <hr830930@dal.ca>
2023-06-02 17:38:57 -0300

    Login Signup Chakra UI Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import AppRouter from './router';
 
 function App() {
   return (
-    <div className="App">
-      <button class='btn btn-primary'>TEsting</button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppRouter />
   );
 }
 
