a4d0f56439f490447442c246f4d917c1d8a583a2
Harshil Kalpeshkumar Shah <hr830930@dal.ca>
2023-06-04 12:27:20 -0300

    converted to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root')
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
