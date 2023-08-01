e425a8f35d1850235646fd0cebeb56699a1f3bed
Harshil <sharshil1299@gmail.com>
2023-08-01 19:20:55 -0300

    check now chatbot should work

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
+import {ChakraProvider} from "@chakra-ui/react";
 
-import { Kommunicate } from "@kommunicate/kommunicate-chatbot-plugin";
+import {Kommunicate} from "@kommunicate/kommunicate-chatbot-plugin";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ChakraProvider>
+            <App/>
+        </ChakraProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
