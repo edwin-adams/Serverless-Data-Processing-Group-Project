6b7b5571b11a0ce296398bba5eac37f820f3da8b
Harshil <sharshil1299@gmail.com>
2023-08-02 19:37:06 -0300

    calls from khushi added

--- src/auth/Dashboard.tsx
+++ src/auth/Dashboard.tsx
@@ -1,8 +1,8 @@
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
-import {Avatar, Box, Button, VStack, Text} from "@chakra-ui/react";
+import {Avatar, Box, Button, Flex, Table, Tbody, Td, Text, Th, Thead, Tr} from "@chakra-ui/react";
 import GameStats, {GameData} from "./GameStates";
-import {fetchData} from "./service/RestCall";
+import {deleteData, fetchData} from "./service/RestCall";
 
 interface ScoreData {
     startTime: number;
@@ -15,11 +15,30 @@ interface ScoreData {
     game_id: string;
 }
 
+interface leader {
+    user_id: string;
+    value: number;
+}
+
+const initialLeaderState: leader = {
+    user_id: 'user_1',
+    value: 0,
+};
+
+
 const Dashboard = () => {
     const [gameData, setGameData] = useState<GameData | null>(null);
     const [imageUrl, setImageUrl] = useState('');
     const [teamAffiliations, setTeamAffiliations] = useState<Set<string>>(new Set<string>());
     const [loggedUser, setLoggedUser] = useState(JSON.parse(localStorage.getItem('user')));
+
+    const [topScorer, setTopScore] = useState<leader>(initialLeaderState);
+    const [topWinLoss, setTopWinLoss] = useState<leader>(initialLeaderState);
+    const [topWin, setTopWin] = useState<leader>(initialLeaderState);
+    const [topLoss, setTopLoss] = useState<leader>(initialLeaderState);
+
+    const DELETE_khushideletefromteam: string = '';
+    const GET_khushiTeamsByUserID: string = '';
 
     useEffect(() => {
         setLoggedUser(JSON.parse(localStorage.getItem('user')));
@@ -31,10 +50,69 @@ const Dashboard = () => {
 
         fetchData(GET_mohituserdata).then((res) => {
             console.log(res);
-            console.log(calculateWinLossAndTotalScore(res));
-            // setGameData(data);
+            calculateWinLossAndTotalScore(res);
         })
+
+
+        for (let i = 1; i < 10; i++) {
+            let GET_mohituserdata2 = 'https://rv7nzfzjhc.execute-api.ca-central-1.amazonaws.com/Prod/getScore?userId=user_' + i;
+            fetchData(GET_mohituserdata2).then((res) => {
+                console.log(res);
+                getAchievements(res);
+            })
+        }
+
     }, []);
+
+
+    const getTeamsByUserId = async () => {
+        fetchData(GET_khushiTeamsByUserID + '?user_email=' + loggedUser.email).then((res) => {
+            console.log(res);
+            setTeamAffiliations(res);
+        });
+    }
+
+    const getAchievements = (scores: ScoreData[]) => {
+        let winCount = 0;
+        let lossCount = 0;
+        let totalScore = 0;
+        let cu_user_id: string = '';
+        scores.forEach((score) => {
+            if (score.status === 'win') {
+                winCount++;
+            } else {
+                lossCount++;
+            }
+
+            totalScore += score.score;
+            cu_user_id = score.user_id;
+        });
+        if (topWin.value < winCount) {
+            setTopWin({user_id: cu_user_id, value: winCount})
+        }
+        if (topLoss.value > lossCount) {
+            setTopLoss({user_id: cu_user_id, value: lossCount})
+        }
+        if (topWinLoss.value < winCount / lossCount) {
+            setTopWinLoss({user_id: cu_user_id, value: winCount / lossCount})
+        }
+        if (topScorer.value < totalScore) {
+            setTopScore({user_id: cu_user_id, value: totalScore})
+        }
+        return {winCount, lossCount, totalScore};
+    }
+
+    const handleButtonClick = (team_id: string) => {
+        const payload = {
+            "user_email": loggedUser.email,
+            "team_id": team_id
+        }
+        deleteData(DELETE_khushideletefromteam, payload).then((res) => {
+            console.log(res);
+        })
+    }
+
+
     const calculateWinLossAndTotalScore = (scores: ScoreData[]) => {
         let winCount = 0;
         let lossCount = 0;
@@ -92,12 +170,51 @@ const Dashboard = () => {
             <Text fontSize="lg" fontWeight="bold">
                 Team Affiliations
             </Text>
-
             {Array.from(teamAffiliations).map((number: string) => (
-                <Text key={number}>{number}</Text>
-            ))}
+                <Flex key={number} align="left">
+                    <p>{number}</p> &nbsp;&nbsp;&nbsp;
+                    <Button onClick={() => handleButtonClick(number)} mb={2}>Leave Team</Button>
+                </Flex>)
+            )}
+
+            <Text fontSize="lg" fontWeight="bold" mt={3}>
+                Top Achievers
+            </Text>
+
+            <Table variant="striped" colorScheme="teal" mb={4}>
+                <Thead>
+                    <Tr>
+                        <Th></Th>
+                        <Th>User ID</Th>
+                        <Th>Achievement</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    <Tr>
+                        <Th>Lowest Loss</Th>
+                        <Td>{topLoss.user_id}</Td>
+                        <Td>{topLoss.value}</Td>
+                    </Tr>
+                    <Tr>
+                        <Th>Highest Win</Th>
+                        <Td>{topWin.user_id}</Td>
+                        <Td>{topWin.value}</Td>
+                    </Tr>
+                    <Tr>
+                        <Th> Highest Win/Loss</Th>
+                        <Td>{topWinLoss.user_id}</Td>
+                        <Td>{topWinLoss.value}</Td>
+                    </Tr>
+                    <Tr>
+                        <Th>Top Scorer</Th>
+                        <Td>{topScorer.user_id}</Td>
+                        <Td>{topScorer.value}</Td>
+                    </Tr>
+                </Tbody>
+            </Table>
         </>
-    );
+    )
+        ;
 }
 
 export default Dashboard;
